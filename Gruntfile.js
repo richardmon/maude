f9fbfb56ca45a6938db0988b5b4617a238c41b86
Richard <losmontoya93@gmail.com>
2015-12-21 17:39:08 -0500

    First functional-ish web page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,117 @@
 module.exports = function(grunt){
 
   grunt.initConfig({
+    //Jade compilation
     jade: {
       compile: {
 	options: {
 	  pretty: true,
 	},
 	files: {
-	  "dist/index.html": ["app/**/**.jade"],
+	  "app/index.html": ["app/**/**.jade"],
 	}
       }
     },
+    //Look for changes
     watch:{
-      // grunt:{ files: ['Gruntfile.js'] },
       jade:{
-	files: ['app/**/*.jade'],
+	files: ['app/**/**.jade'],
 	tasks: ['jade']
+      },
+      stylus:{
+        files: ['app/_styles/*.styl'],
+        tasks: ['stylus']
       }
     },
+    //Stylus
+    stylus: {
+      compile: {
+        options: {
+          compress: false
+        },
+        files: {
+          'app/styles/style.css' : 'app/_styles/*.styl'
+        }
+      }
+    },
+  //Server
     connect: {
       server: {
 	options: {
 	  livereload: true,
-	  base: 'dist/',
+	  base: 'app/',
 	  port: 8000
 	}
       }
-    }
+    },
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({browsers: ['> 0.5%', 'last 5 versions', 'Firefox > 3', 'ie > 8', 'Opera > 9']}) // add vendor prefixes
+        ]
+      },
+      dist: {
+        files: {
+          'app/styles/style.css': 'app/styles/style.css'
+        }
+      }
+    },
+    // //Concat Files
+    // concat: {
+    //   javascript: {
+	  // dest: 'dist/js/script.js',
+	  // src: [
+	    // 'app/scripts/**/*.js',
+	    // 'app/components/**/*.min.js'
+	    // ]
+    //   },
+    //   css:{
+	// dest: 'dist/css/style.css',
+	// src: ['app/style/**/*.css',
+	      // 'app/components/**/*.min.css']
+    //   }
+    // },
+    // //Minimize javascripts
+    // uglify: {
+    //   generated: {
+	// files: [
+	  // {
+	    // dest: 'dist/js/script.min.js',
+	    // src: [ 'dist/js/script.js' ]
+	  // }
+	// ]
+    //   }
+    // },
+    // //Minified css
+    // cssmin: {
+    //   my_target: {
+	  // src: 'dist/css/style.css',
+	  // dest: 'dist/css/style.min.css'
+    //   }
+    // },
+    // useminPrepare: {
+    //   html: 'dist/index.html'
+    // },
+    // usemin:{
+    //   html:['dist/index.html']
+    // }
   });
 
+  //Load
   grunt.loadNpmTasks('grunt-contrib-jade');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-postcss');
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  // grunt.loadNpmTasks('grunt-css');
+  // grunt.loadNpmTasks('grunt-usemin');
 
+
+  //Tasks
   grunt.registerTask('build', 'Convert Jade templates into html templates', ['jade']);
-  grunt.registerTask('default', ['jade', 'connect:server','watch']);
+  grunt.registerTask('default', ['stylus', 'postcss', 'jade', 'connect:server', 'watch']);
+  // grunt.registerTask('default', ['jade', 'concat', 'uglify', 'cssmin', 'useminPrepare', 'usemin', 'connect:server','watch']);
 
 }
---
+++ app/scripts/functions.js
@@ -0,0 +1,37 @@
+(function(){
+
+  if(! /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){ //It's not a phone
+    $(window).scroll(function(){
+
+      var scroll = $(this).scrollTop();
+
+      //Navbar effect
+
+      var wHeight = $(window).height();
+      if(scroll > wHeight*0.4){
+        $('.navbar').addClass('affix');
+      }
+      else{
+        $('.navbar').removeClass('affix');
+      }
+
+      //Parallax Header
+        $('.header-content').css({
+          'transform' : 'translate(0px, '+ scroll/3 + '%)'
+        });
+    })//End scroll
+  }
+  //Center the footer
+  var footerCentered =function(){
+    var footerWidth = $('.footer-content').outerWidth();
+    var footerHeight = $('.footer-content').outerHeight();
+    $('.footer-content').css({
+      'left':'calc(50% - ' + footerWidth/2 + 'px)'
+    });
+    $('footer').css({
+      'height' : footerHeight + 20 + 'px'
+    });
+  }
+  $(window).ready(footerCentered);
+  $(window).resize(footerCentered);
+})();
